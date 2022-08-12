379bd4f8f01193b6433c5b23290927b0b7f782fb
Divyansh Rathi <divyanshrathi0404@gmail.com>
2022-08-12 17:00:10 +0530

    Updates

---
+++ precache-manifest.53300c65003c8d508dd8311448e62d97.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb74dd4abffdd539863ad3fa512a65a4",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "9a709b310fdc68b54d81",
+    "url": "/covid-tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "9492c8ccee9bcfd8c490",
+    "url": "/covid-tracker/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "9a709b310fdc68b54d81",
+    "url": "/covid-tracker/static/js/2.e2750657.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/covid-tracker/static/js/2.e2750657.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9492c8ccee9bcfd8c490",
+    "url": "/covid-tracker/static/js/main.1442937d.chunk.js"
+  },
+  {
+    "revision": "1af475054be274cc1e5c",
+    "url": "/covid-tracker/static/js/runtime-main.9cfae146.js"
+  }
+]);
